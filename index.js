203a77ca1086f7022b7fe7dd421ce3ad71d3c90a
stay <nagyzeteny6@gmail.com>
2022-07-20 12:22:21 +0200

    simplifying code

--- index.js
+++ index.js
@@ -3,17 +3,16 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const WakeUpDynos = async () => {
-	Object.entries(process.env).forEach((entry) => {
-		if (!entry[0].startsWith('URL')) return;
-		const url = entry[1];
+  Object.entries(process.env).forEach((entry) => {
+    if (!entry[0].startsWith('URL')) return;
 
-		try {
-			axios.get(url);
-			console.log(`Dyno(${url}) has been successfully woken up, at ${date}`);
-		} catch {
-			console.log(`Waking Up Dyno(${url}) failed, at ${date}`);
-		}
-	});
+    try {
+      await axios.get(url);
+      console.log(`Dyno(${entry[1]}) has been successfully woken up, at ${new Date()}`);
+    } catch {
+      console.error(`Waking Up Dyno(${entry[1]}) failed, at ${new Date()}`);
+    }
+  });
 };
 
 setInterval(WakeUpDynos, 2 * 60 * 1000);
